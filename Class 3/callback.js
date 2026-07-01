d1c8bb8ad861cee65e79b516032011ffe8872b32
aryanishan <myteam034221@gmail.com>
2026-07-01 16:22:24 +0530

    callback 1 class

--- Class 3/callback.js
+++ Class 3/callback.js
@@ -9,3 +9,17 @@
 
 // greet("Aryan", sayBye);
 
+function placeOrder(customerName, callback){
+  console.log(`${customerName} is placing an order...`);
+
+  setTimeout(() => {
+    const order = {
+      id: 101, 
+      customer: customerName,
+      items: ["Burger", "Pizza" ,"Cold Drink"]
+    };
+
+    console.log(`Order #${order.id} placed successfully.`);
+    callback(order);
+  }, 2000);
+}
