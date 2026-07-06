c37692bccd7c3b91122010219600be0678bbfedf
aryanishan <myteam034221@gmail.com>
2026-07-06 15:25:33 +0530

    Counter

--- Class 5/my-react-app/src/App.jsx
+++ Class 5/my-react-app/src/App.jsx
@@ -2,11 +2,18 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0);
+
 
   return (
     <>
       <h1>Welcome to My React App</h1>
       <p>This is a simple React app.</p>
+      <p>Counter</p>
+      <p>{count}</p>
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+      <button onClick={() => setCount(count + 1)}>+</button>
     </>
   )
 }
