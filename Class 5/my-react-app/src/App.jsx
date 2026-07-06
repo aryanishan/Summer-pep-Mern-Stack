0003166679a5b72de4dc697242058142b42d95fa
aryanishan <myteam034221@gmail.com>
2026-07-06 14:43:52 +0530

    React started

---
+++ Class 5/my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Welcome to My React App</h1>
+      <p>This is a simple React app.</p>
+    </>
+  )
+}
+
+export default App
