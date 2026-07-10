c3002fb31aa3c39e7f99f27690f82edc4c59c706
aryanishan <myteam034221@gmail.com>
2026-07-10 16:33:48 +0530

    dummy header and footer has been added

---
+++ Class 6/my-app/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Copyright 2026 DummyBrand. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Class 6/my-app/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <header className="navbar">
+      <div className="brand">DummyBrand</div>
+
+      <nav className="nav-links">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
