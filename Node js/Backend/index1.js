37c3121b27463d6f6df9ce921d2463b3cdc3fec9
aryanishan <myteam034221@gmail.com>
2026-07-17 16:33:15 +0530

    backend practice

---
+++ Node js/Backend/index1.js
@@ -0,0 +1,18 @@
+const express = require('express');
+require('dotenv').config();
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World'
+  });
+  return;
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+
+})
